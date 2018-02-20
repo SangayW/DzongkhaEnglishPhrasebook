5e6f90422ef66d878317e0e3785d9c5d074746fd
sangay <sanglim2012@gmail.com>
2018-02-20 22:27:58 +0600

    added logo and changed the database

---
+++ app/src/main/java/bt/gov/dzongkha/dzongkhaenglishphrasebook/CategoryDetails/CategoryDetails.java
@@ -0,0 +1,223 @@
+package bt.gov.dzongkha.dzongkhaenglishphrasebook.CategoryDetails;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import javax.mail.AuthenticationFailedException;
+import javax.mail.MessagingException;
+
+import bt.gov.dzongkha.dzongkhaenglishphrasebook.FeedBackActivity.Mail;
+import bt.gov.dzongkha.dzongkhaenglishphrasebook.R;
+
+public class CategoryDetails extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    String Message,Password,Email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Toolbar toolbar1 = (Toolbar) findViewById(R.id.toolbar1);
+        LayoutInflater li = LayoutInflater.from(this);
+        View customView = li.inflate(R.layout.custom_layout, null);
+        toolbar1.addView(customView);
+
+        ImageButton back=customView.findViewById(R.id.back);
+       // ImageButton play=customView.findViewById(R.id.playAll);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.category_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Fragment fragment = null;
+        if (id == R.id.nav_setting) {
+            // Handle the camera action
+        } else if (id == R.id.nav_fav) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_rate) {
+
+        } else if (id == R.id.nav_feedback) {
+            giveFeedback();
+        } else if (id == R.id.nav_about) {
+
+        }else if (id == R.id.nav_exit) {
+
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            // ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void giveFeedback() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.activity_feed_back, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText message = dialogView.findViewById(R.id.message);
+        final EditText password=dialogView.findViewById(R.id.password);
+        final EditText email=dialogView.findViewById(R.id.email);
+
+        dialogBuilder.setTitle("Feed back");
+        dialogBuilder.setMessage("Please give us the feed back about the app");
+        dialogBuilder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                Message=message.getText().toString();
+                Password=password.getText().toString();
+                Email=email.getText().toString();
+
+                sendMessage();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //pass
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        b.getButton(b.BUTTON_NEGATIVE).setTextColor(Color.RED);
+        b.getButton(b.BUTTON_POSITIVE).setTextColor(Color.BLACK);
+
+    }
+    private void sendMessage() {
+        String[] recipients = { "sanglim2012@gmail.com" };
+        SendEmailAsyncTask email1 = new SendEmailAsyncTask();
+
+        email1.m = new Mail(Email, Password);
+        email1.m.set_from(Email);
+        email1.m.setBody(Message);
+        email1.m.set_to(recipients);
+        email1.m.set_subject("Feedback");
+        email1.execute();
+    }
+
+    class SendEmailAsyncTask extends AsyncTask<Void, Void, Boolean> {
+        Mail m;
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+
+            if(aBoolean){
+                Toast.makeText(getApplicationContext(),"feedback sent successfully",Toast.LENGTH_LONG).show();
+            }
+            else
+                Toast.makeText(getApplicationContext(),"feedback sending failed",Toast.LENGTH_LONG).show();
+        }
+
+        public SendEmailAsyncTask() {}
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            try {
+                if(m.send()){
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            catch (AuthenticationFailedException e) {
+                Log.e(SendEmailAsyncTask.class.getName(), "Bad account details");
+                e.printStackTrace();
+                return false;
+            }
+            catch (MessagingException e) {
+                Log.e(SendEmailAsyncTask.class.getName(), "Email failed");
+                e.printStackTrace();
+                return false;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+
+
+
+}
